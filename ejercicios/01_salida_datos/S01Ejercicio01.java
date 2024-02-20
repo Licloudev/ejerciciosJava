1537bb2c5548bd8ed7feab143da7d90b54985bc0
Claudio Martinez <claudio.martinez.it@hbplat.com>
2024-02-20 12:24:37 +0100

    first commit

---
+++ ejercicios/01_salida_datos/S01Ejercicio01.java
@@ -0,0 +1,13 @@
+
+/**
+ * 1. Salida por pantalla
+ *
+ * 1. Escribe una programa que muestre tu nombre por pantalla.
+ *
+ *
+ */
+public class S01Ejercicio01 {
+    public static void main(String[] args) {
+        System.out.println("TU NOMBRE");
+    }
+}
